63a53f70b06d0dae7b84e366ea94698f63ff9744
bindu325 <himabindugade0706@gmail.com>
2026-01-26 21:48:34 +0530

    created routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+// import "./index.css";
 
 // import { StrictMode } from 'react'
 // import { createRoot } from 'react-dom/client'
@@ -19,6 +19,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
