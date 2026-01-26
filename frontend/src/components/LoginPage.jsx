f4dccf3e1a19edc350cf20b2a751f9b77ed5f5d8
Bhavana-1006 <angirekulabhavana@gmail.com>
2026-01-26 19:19:29 +0530

    LoginPage

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-
-const LoginPage = () => {
+export default function LoginPage() {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="login-container">
+      <h2>Sign in</h2>
+      <p className="subtitle">Access your service dashboard securely</p>
 
-export default LoginPage
+      <input type="email" placeholder="Email" />
+      <input type="password" placeholder="Password" />
+
+      <button>Login</button>
+
+      <div className="links">
+        <a href="#">Forgot password?</a>
+        <a href="#">Get Started</a>
+      </div>
+    </div>
+  );
+}
